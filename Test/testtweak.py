6d5361e6ea037af69ac42b854b5d4ebfc49f99a7
MrBenGriffin <ben@redsnapper.net>
2019-08-10 20:14:14 +0100

    Added test for tweak.entry.

--- Test/testtweak.py
+++ Test/testtweak.py
@@ -1,5 +1,8 @@
 import unittest
+# import math
+
 from knot.works import Tweak, Tw, Com, Dim
+
 
 class TestTweak(unittest.TestCase):
     tweaks = (Tw.master, Tw.horizon, Tw.vanity, Tw.mirror, Tw.rot000, Tw.rot090, Tw.rot270, Tw.rot180)
@@ -7,6 +10,9 @@ class TestTweak(unittest.TestCase):
 
     def test_str(self):
         self.assertEqual(str(Tweak(Tw.horizon, 4, 4)), "Horizon", "Should be Horizon")
+
+    def test_repr(self):
+        self.assertEqual(repr(Tweak(Tw.horizon, 3, 4)), "Horizon; Dim(x=2, y=3)", "Should be Horizon; Dim(x=2, y=3)")
 
     def test_face(self):
         answer_set = {
@@ -40,10 +46,13 @@ class TestTweak(unittest.TestCase):
         }
         for x in range(2):
             for y in range(2):
-                dim = Dim(x, y)
+                dxy = Dim(x, y)
                 for tweak in self.tweaks:
                     test = Tweak(tweak, 2, 2)
-                    self.assertEqual(test.dim(dim), answers[tweak][x*2+y], "Dim " + str(tweak) + ":" + str(dim) + " expected " + str(answers[tweak][x*2+y]))
+                    part_a = test.dim(dxy)
+                    part_b = answers[tweak][x*2+y]
+                    message = "Dim " + str(tweak) + ":" + str(dxy) + " expected " + str(answers[tweak][x*2+y])
+                    self.assertEqual(part_a, part_b, message)
 
     def test_dim_odd(self):
         answers = {
@@ -55,13 +64,47 @@ class TestTweak(unittest.TestCase):
             Tw.rot180:  (Dim(2, 2), Dim(2, 1), Dim(2, 0), Dim(1, 2), Dim(1, 1), Dim(1, 0), Dim(0, 2), Dim(0, 1), Dim(0, 0)),
             Tw.rot090:  (Dim(0, 2), Dim(1, 2), Dim(2, 2), Dim(0, 1), Dim(1, 1), Dim(2, 1), Dim(0, 0), Dim(1, 0), Dim(2, 0)),
             Tw.rot270:  (Dim(2, 0), Dim(1, 0), Dim(0, 0), Dim(2, 1), Dim(1, 1), Dim(0, 1), Dim(2, 2), Dim(1, 2), Dim(0, 2))
-       }
+        }
         for x in range(3):
             for y in range(3):
                 dim = Dim(x, y)
                 for tweak in self.tweaks:
                     test = Tweak(tweak, 3, 3)
-                    self.assertEqual(test.dim(dim), answers[tweak][x*3+y], "Dim " + str(tweak) + ":" + str(dim) + " expected " + str(answers[tweak][x*3+y]))
+                    self.assertEqual(test.dim(dim), answers[tweak][x * 3 + y], "Dim " + str(tweak) + ":" + str(dim) + " expected " + str(answers[tweak][x * 3 + y]))
+
+    def test_entry(self):
+        ans0 = {
+            Tw.master:  {4: {4: Dim(2, 2), 5: Dim(2, 2)}, 5: {4: Dim(2, 2), 5: Dim(2, 2)}},
+            Tw.horizon: {4: {4: Dim(2, 1), 5: Dim(2, 2)}, 5: {4: Dim(2, 1), 5: Dim(2, 2)}},
+            Tw.vanity:  {4: {4: Dim(1, 2), 5: Dim(1, 2)}, 5: {4: Dim(2, 2), 5: Dim(2, 2)}},
+            Tw.mirror:  {4: {4: Dim(1, 1), 5: Dim(1, 2)}, 5: {4: Dim(2, 1), 5: Dim(2, 2)}},
+            Tw.rot000:  {4: {4: Dim(2, 2), 5: Dim(2, 2)}, 5: {4: Dim(2, 2), 5: Dim(2, 2)}},
+            Tw.rot090:  {4: {4: Dim(2, 1), 5: Dim(2, 2)}, 5: {4: Dim(2, 1), 5: Dim(2, 2)}},
+            Tw.rot180:  {4: {4: Dim(1, 1), 5: Dim(1, 2)}, 5: {4: Dim(2, 1), 5: Dim(2, 2)}},
+            Tw.rot270:  {4: {4: Dim(1, 2), 5: Dim(1, 2)}, 5: {4: Dim(2, 2), 5: Dim(2, 2)}}
+        }
+        ans23 = {
+            Tw.master:  {4: {4: Dim(2, 1), 5: Dim(2, 1)}, 5: {4: Dim(2, 1), 5: Dim(2, 1)}},
+            Tw.horizon: {4: {4: Dim(2, 2), 5: Dim(2, 3)}, 5: {4: Dim(2, 2), 5: Dim(2, 3)}},
+            Tw.vanity:  {4: {4: Dim(1, 1), 5: Dim(1, 1)}, 5: {4: Dim(2, 1), 5: Dim(2, 1)}},
+            Tw.mirror:  {4: {4: Dim(1, 2), 5: Dim(1, 3)}, 5: {4: Dim(2, 2), 5: Dim(2, 3)}},
+            Tw.rot000:  {4: {4: Dim(2, 1), 5: Dim(2, 1)}, 5: {4: Dim(2, 1), 5: Dim(2, 1)}},
+            Tw.rot090:  {4: {4: Dim(1, 1), 5: Dim(1, 2)}, 5: {4: Dim(1, 1), 5: Dim(1, 2)}},
+            Tw.rot180:  {4: {4: Dim(1, 2), 5: Dim(1, 3)}, 5: {4: Dim(2, 2), 5: Dim(2, 3)}},
+            Tw.rot270:  {4: {4: Dim(2, 2), 5: Dim(2, 2)}, 5: {4: Dim(3, 2), 5: Dim(3, 2)}}
+        }
+        for tweak in self.tweaks:
+            for x in range(4, 6):
+                for y in range(4, 6):
+                    test = Tweak(tweak, x, y)
+                    answer0 = ans0[tweak][x][y]
+                    self.assertEqual(answer0, test.entry(0), str(tweak) + ";" + str(x) + "," + str(y) + ",0: got " + str(test.entry(0)) + " expected " + str(answer0))
+                    answer2 = ans23[tweak][x][y]
+                    self.assertEqual(answer2, test.entry(2), str(tweak) + ";" + str(x) + "," + str(y) + ",2: got " + str(test.entry(2)) + " expected " + str(answer2))
+                    answer3 = ans23[tweak][x][y]
+                    self.assertEqual(answer3, test.entry(3), str(tweak) + ";" + str(x) + "," + str(y) + ",3: got" + str(test.entry(3)) + " expected " + str(answer3))
+                    answer4 = ans0[tweak][x][y]
+                    self.assertEqual(answer4, test.entry(4), str(tweak) + ";" + str(x) + "," + str(y) + ",4: got" + str(test.entry(4)) + " expected " + str(answer4))
 
 
 if __name__ == '__main__':
--- knot/works/tweak.py
+++ knot/works/tweak.py
@@ -8,13 +8,9 @@ from .tw import Tw
 class Tweak:
     """
     Tweak provides the information for clones. It answers the following questions from a clone.
-
     What is my orientation to the other (self.tweak).
-
     If the other faces West, which direction I should face? face()
-
     If the other is at dimension 0,0 what cell I be in?  dim()
-
     Given that the maze might be hollow in the middle, where should I start?
 
     """
@@ -24,10 +20,15 @@ class Tweak:
                 Tw.rot180: "Rotate 180", Tw.rot270: "Rotate 270"}
         return text[self.tweak]
 
+    def __repr__(self):
+        text = {Tw.master: "As is", Tw.horizon: "Horizon", Tw.vanity: "Vanity",
+                Tw.mirror: "Mirror", Tw.rot000: "Rotate 000", Tw.rot090: 'Rotate 090',
+                Tw.rot180: "Rotate 180", Tw.rot270: "Rotate 270"}
+        return text[self.tweak] + "; " + str(self._dim)
+
     def __init__(self, tweak, width, height):
         self.tweak = tweak
-        self.x_off = width - 1
-        self.y_off = height - 1
+        self._dim = Dim(max(0, width - 1), max(0, height - 1))
 
     def face(self, com: Com) -> Com:
         if self.tweak == Tw.master or self.tweak == Tw.rot000:
@@ -52,10 +53,15 @@ class Tweak:
         if self.tweak == Tw.rot090 or self.tweak == Tw.rot270:
             x, y = y, x
         if self.tweak & Tw.vanity != 0:  # not 270, not horizon
-            x = self.x_off - x
+            x = self._dim.x - x
         if self.tweak & Tw.horizon != 0:  # not 90
-            y = self.y_off - y
+            y = self._dim.y - y
         return Dim(x, y)
 
     def entry(self, border: int = 0) -> Dim:
-        return self.dim(Dim(ceil(self.x_off/2), ceil(self.y_off/2) if border is 0 else floor(border/2)))
+        """
+        Given that the structure might be hollow, where should an actor start?
+        my x_off and y_off are the maximum indices of a grid.
+        Normally I start at the centre of the grid at ceil((grid.x-1)/2),ceil((grid.x-1)/2)
+        """
+        return self.dim(Dim(ceil(self._dim.x / 2), ceil(self._dim.y / 2) if border is 0 else floor(border / 2)))
